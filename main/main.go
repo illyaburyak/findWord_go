0719bd7c556dd99b95e984417db625038bced64b
Illya Buryak <illyabenavente@gmail.com>
2021-07-09 17:25:34 -0500

    "new version:1.0"

--- main/main.go
+++ main/main.go
@@ -1,118 +1,119 @@
-//package main
-//
-//import (
-//	"fmt"
-//	"github.com/PuerkitoBio/goquery"
-//
-//	"net/http"
-//	"strings"
-//)
-//
-//type findWord int
-//
-//// This func is going to write back respond
-//func (m findWord) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-//
-//	fmt.Println("Enter Word you wanna search for ")
-//	var userWord string
-//	fmt.Scanln(&userWord)
-//
-//	url := req.URL
-//
-//	newUrl := url.String()
-//	//get html
-//	doc, err := goquery.NewDocumentFromReader(strings.NewReader(newUrl))
-//	if err != nil {
-//		panic(err)
-//	}
-//
-//	// find user name
-//	doc.Find(userWord).Each(func(_ int, node *goquery.Selection) {
-//		fmt.Print("Your word is " + userWord)
-//	})
-//
-//}
-//
-//
-//func main() {
-//	// display output in the next line
-//	fmt.Println("Enter URL please: ")
-//	var userUrl string
-//
-//	// Storing input from user
-//	fmt.Scanln(&userUrl)
-//
-//
-//	var searchingFunc findWord
-//	err := http.ListenAndServe(userUrl, searchingFunc)
-//	if err != nil {
-//		return
-//	}
-//
-//}
-
-package main
+package lol
 
 import (
-	"fmt"
-	"github.com/PuerkitoBio/goquery"
-	"io"
-	"io/ioutil"
+	"encoding/json"
 	"log"
-	"net/http"
+	"os"
 	"strings"
+
+	"github.com/gocolly/colly/v2"
 )
 
+// Course stores information about a coursera course
+type Course struct {
+	Title       string
+	Description string
+	Creator     string
+	Level       string
+	URL         string
+	Language    string
+	Commitment  string
+	Rating      string
+}
+
 func main() {
-
-	// get user url
-	fmt.Println("Enter URL please: ")
-	var userUrl string
-
-	// get user word
-	fmt.Println("Enter you word please: ")
-	var userWord string
-
-	// trim spaces
-	userWord = strings.TrimSpace(userWord)
-
-	// fetch data
-	resp, err := http.Get(userUrl)
-
+	fName := "courses.json"
+	file, err := os.Create(fName)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Cannot create file %q: %s\n", fName, err)
+		return
 	}
+	defer file.Close()
 
-	// close body when finish
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
+	// Instantiate default collector
+	c := colly.NewCollector(
+		// Visit only domains: coursera.org, www.coursera.org
+		colly.AllowedDomains("coursera.org", "www.coursera.org"),
 
+		// Cache responses to prevent multiple download of pages
+		// even if the collector is restarted
+		colly.CacheDir("./coursera_cache"),
+	)
+
+	// Create another collector to scrape course details
+	detailCollector := c.Clone()
+
+	courses := make([]Course, 0, 200)
+
+	// On every <a> element which has "href" attribute call callback
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		// If attribute class is this long string return from callback
+		// As this a is irrelevant
+		if e.Attr("class") == "Button_1qxkboh-o_O-primary_cv02ee-o_O-md_28awn8-o_O-primaryLink_109aggg" {
+			return
 		}
-	}(resp.Body)
-
-	// read content
-	body, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-
-		log.Fatal(err)
-	}
-
-	// storing html
-	htmlPage := string(body)
-
-	// get dom
-	doc, err := goquery.NewDocumentFromReader(strings.NewReader(htmlPage))
-	if err != nil {
-		panic(err)
-	}
-
-	// find user word
-	doc.Find(userWord).Each(func(_ int, node *goquery.Selection) {
-		fmt.Print("Your word is " + userWord)
-		ret := node.Find(userWord)
-		fmt.Println(ret)
+		link := e.Attr("href")
+		// If link start with browse or includes either signup or login return from callback
+		if !strings.HasPrefix(link, "/browse") || strings.Index(link, "=signup") > -1 || strings.Index(link, "=login") > -1 {
+			return
+		}
+		// start scaping the page under the link found
+		e.Request.Visit(link)
 	})
 
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		log.Println("visiting", r.URL.String())
+	})
+
+	// On every <a> element with collection-product-card class call callback
+	c.OnHTML(`a.collection-product-card`, func(e *colly.HTMLElement) {
+		// Activate detailCollector if the link contains "coursera.org/learn"
+		courseURL := e.Request.AbsoluteURL(e.Attr("href"))
+		if strings.Index(courseURL, "coursera.org/learn") != -1 {
+			detailCollector.Visit(courseURL)
+		}
+	})
+
+	// Extract details of the course
+	detailCollector.OnHTML(`div[id=rendered-content]`, func(e *colly.HTMLElement) {
+		log.Println("Course found", e.Request.URL)
+		title := e.ChildText(".banner-title")
+		if title == "" {
+			log.Println("No title found", e.Request.URL)
+		}
+		course := Course{
+			Title:       title,
+			URL:         e.Request.URL.String(),
+			Description: e.ChildText("div.content"),
+			Creator:     e.ChildText("li.banner-instructor-info > a > div > div > span"),
+			Rating:      e.ChildText("span.number-rating"),
+		}
+		// Iterate over div components and add details to course
+		e.ForEach(".AboutCourse .ProductGlance > div", func(_ int, el *colly.HTMLElement) {
+			svgTitle := strings.Split(el.ChildText("div:nth-child(1) svg title"), " ")
+			lastWord := svgTitle[len(svgTitle)-1]
+			switch lastWord {
+			// svg Title: Available Langauges
+			case "languages":
+				course.Language = el.ChildText("div:nth-child(2) > div:nth-child(1)")
+			// svg Title: Mixed/Beginner/Intermediate/Advanced Level
+			case "Level":
+				course.Level = el.ChildText("div:nth-child(2) > div:nth-child(1)")
+			// svg Title: Hours to complete
+			case "complete":
+				course.Commitment = el.ChildText("div:nth-child(2) > div:nth-child(1)")
+			}
+		})
+		courses = append(courses, course)
+	})
+
+	// Start scraping on http://coursera.com/browse
+	c.Visit("https://coursera.org/browse")
+
+	enc := json.NewEncoder(file)
+	enc.SetIndent("", "  ")
+
+	// Dump json to the standard output
+	enc.Encode(courses)
 }
